9e01194e2d1c45986ea41b752f0c85c57686025e
rumezaa <97740994+rumezaa@users.noreply.github.com>
2022-04-23 16:08:07 -0600

    new commits

--- app.py
+++ app.py
@@ -1,27 +1,61 @@
+import eventbrite
+import requests
 from flask import Flask, url_for
 from flask import render_template, redirect, request
-
+from handle_events.handle_calendar import HandleCalendar as HC
+from handle_events.handle_events import Events
+import json
 
 app = Flask(__name__)
 
+key = "SIPL2IVAEKK6BOEFS3"
+cl_sec = "S7WZZRPSIBMWSMSGERQ65OSBLUKBOXA5ZM7UYSEQTA2PZ7NJLM"
+end_point = "http://127.0.0.1:5000/home"
+
 @app.route('/')
 def main():  # put application's code here
-    end_point = ""
-    return render_template('index.html',**locals())
+    return render_template('login.html', **locals())
 
 
 @app.route('/auth')
 def auth():
-    key = "SIPL2IVAEKK6BOEFS3"
-    return redirect(f"https://www.eventbrite.com/oauth/authorize?response_type=token&client_id={key}&redirect_uri={url_for('home')}")
+    #authorization
+    return redirect(f"https://www.eventbrite.com/oauth/authorize?response_type=token&client_id={key}&redirect_uri={end_point}")
 
 @app.route('/home', methods=['GET','POST'])
-def vew_cal():
-    if request.method == 'POST':
+def view_cal():
+    #------------------------------ this code does not work at the moment but can be used for future reference -----------------------------------------------#
+    code = request.args.get("access_token")
+    code = "HMDILKOOYVHWM4HOFU7J"
+
+    data = {"grant_type": "authorization_code",'client_id':key,"client_secret": cl_sec,"code": code,"redirect_uri":end_point}
+
+    header = {
+        'content-type': 'application/x-www-form-urlencoded'
+    }
+    cl_id = requests.post('https://www.eventbrite.com/oauth/token',json=data, headers=header)
+
+
+    try:
+        Events.get_events(cl_id)
+    except:
         pass
 
-    return render_template('main.html')
+    #-------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
+    #post events via button
+    if request.method == 'POST':
+        HC().add_events()
+
+    return render_template('home.html')
+
+
+
+
+
+@app.route('/view')
+def cal():
+    return render_template('view.html')
 
 
 
