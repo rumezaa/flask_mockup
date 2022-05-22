1b6ce83bb2cea2ee18b24120798fbf998db7b9fd
rumeza <97740994+rumezaa@users.noreply.github.com>
2022-05-22 10:15:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,7 @@ app = Flask(__name__)
 key = "SIPL2IVAEKK6BOEFS3"
 cl_sec = "S7WZZRPSIBMWSMSGERQ65OSBLUKBOXA5ZM7UYSEQTA2PZ7NJLM"
 end_point = "http://127.0.0.1:5000/home"
-
-
-
-
-
-
-#diff locations on out website
+  
 
 
 #main route
@@ -26,25 +20,26 @@ end_point = "http://127.0.0.1:5000/home"
 def main():  # put application's code here
     return render_template('login.html', **locals())
 
+#authorization
 @app.route('/auth')
 def auth():
     #authorization
     return redirect(f"https://www.eventbrite.com/oauth/authorize?response_type=token&client_id={key}&redirect_uri={end_point}")
 
-
+#intial step in verifying auth and getting personal acess token
 @app.route('/home', methods=['POST','GET'])
 def oauth2():
+    #gets the token from url via js and rediects to main screen
     return '''  <script type="text/javascript">
                 var token = window.location.href.split("access_token=")[1]; 
                 window.location = "/main/" + token;
             </script> '''
 
+#main page w/ calendar and w/ acesc token being used as variable in route
 @app.route('/main/<token>', methods=['GET','POST'])
 def view_cal(token):
+    #if method pot, get eventbrite vents using token and add events via GCal
     if request.method == 'POST':
-        #getting eventbrite events
-
-
         #add events to GCal
         GCal().add_events(Events(token).get_events())
 
