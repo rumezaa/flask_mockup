e80b125a3a49d6e494683772b9faadec754da0f5
Peter Rojkovski <84549374+PeterRojkovski@users.noreply.github.com>
2022-05-27 15:19:58 -0600

    finally (#6)

---
+++ frontend/src/components/SuccessBox.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import './SuccessBox.css';
+import Button from '@mui/material/Button';
+
+function SuccessBox(){
+    const [style, setStyle] = useState("box");
+    const [blur, setBlur] = useState('blur')
+    const [total, setTotal] = useState('total')
+    const changeStyle = () => {
+        setStyle("box2");
+        setBlur("blur2")
+        setTimeout(function(){setStyle("box3")}, 250)
+        setTimeout(function(){setTotal('total2')}, 1000)
+        
+    }
+    return(
+
+        <div className={total}>
+            <div className={blur}></div>
+            <div className={style}>
+                <h1 id='title'>SUCCESS!</h1>
+                <h2>You have successfully signed into eventbrite!</h2>
+                <Button variant="contained" color='success' id='button' onClick={changeStyle}>
+                    Continue to the Calendar
+                </Button>
+            </div>
+        </div>
+
+    )
+}
+
+export default SuccessBox
