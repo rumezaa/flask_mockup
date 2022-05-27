ca7b5a2db89d09c80ace8d911483e7a0f9aebcfd
aliyahMerritt <103625076+aliyahMerritt@users.noreply.github.com>
2022-05-27 15:21:50 -0600

    Commit (#2)
    Co-authored-by: aliyahMerritt <amerritt1234@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { amber, orange } from '@mui/material/colors';
+import { ThemeProvider } from '@emotion/react';
+import { Grid, Typography } from '@mui/material';
+import { Paper } from '@mui/material';
+import { Button } from '@mui/material';
+import { useTheme } from "@mui/material"
+import { Box } from '@mui/system';
+import ParticleBackground from './Components-Welcome/Particle';
+import Icon from './Components-Welcome/EventbriteIcon.PNG'
+import TechConnectTitle from './Components-Welcome/YYC-Tech-Connect.PNG'
+import Theme from './Components-Welcome/Theme.js'
 
 function App() {
+  const theme = useTheme();
+  console.log(theme.palette.main)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <ThemeProvider theme={Theme}>
+      <header className='App-header'>
+        <ParticleBackground></ParticleBackground>
+        <Typography variant="h2" component="div" sx={{ position: "absolute", fontFamily: 'Source Code Pro', top: 50, color: amber[500] }}>
+          Welcome!
+        </Typography>
+        <Typography variant="h4" component="div" sx={{ position: "absolute", width: 500, fontFamily: 'Source Code Pro', top: 160, color: amber[300], fontSize: 20 }}>
+          Welcome to YYC-Tech-Connect - the place where all your Calgary tech events are organized.
+          Sign in with Eventbrite and connect your Google Calendar below.
+        </Typography>
+        <div className="App">
+          <Grid container
+            justifyContent={"center"}>
+            <Paper elevation={24} sx={{ borderRadius: 20, position: "absolute", top: 350 }} className={theme.palette.main}>
+              <Paper style={{ height: 360, width: 350, borderRadius: 20, backgroundColor:amber[500] }}>
+                <Box sx={{ height: 40, width: 40 }}>
+                </Box>
+                <img src={TechConnectTitle} height={75} width={310} />
+                <Box sx={{ height: 30, width: 40 }}>
+                </Box>
+                <Box
+                  justifyContent="center"
+                  alignItems='center'
+                  m={2}>
+                  <Button href='https://www.eventbrite.ca/signin/'
+                    variant='contained' color="secondary" sx={{ mt: 2, ml: 0, height: 60, width: 310, borderRadius: 8 }}>
+                    <Box sx={{ position: "absolute", left: "3%", top: 13 }}>
+                      <img src={Icon} height={30} width={35} />
+                    </Box>
+                    <Typography color={orange[900]} sx={{ fontWeight: 600 }}>
+                      Sign in with Eventbrite
+                    </Typography>
+                  </Button>
+                  <Button href='https://calendar.google.com/calendar/'
+                    variant='contained' color="secondary" sx={{ mt: 2, ml: 0, height: 45, width: 310, borderRadius: 8 }}>
+                    <Typography color={orange[900]} sx={{ fontWeight: 600 }}>
+                      Google Calendar
+                    </Typography>
+                  </Button>
+                </Box>
+              </Paper>
+            </Paper>
+          </Grid>
+        </div >
       </header>
-    </div>
+    </ThemeProvider >
   );
 }
 
